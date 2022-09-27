481469418c011dac439230c7c64952b244440611
kieferjackson <kieferleejackson@gmail.com>
2022-09-26 20:40:06 -0700

    Setup basic controller routing

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+// TODO: include models here and change directory path if necessary >>> const { User } = require('../models');
+
+// TODO: POST a new user
+
+// TODO: POST a user's login request
+
+// TODO: POST a user's logout request
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+// TODO: include models here and change directory path if necessary >>> const { User, Pet } = require('../models');
+
+// GET all users including basic pet data (e.g. Username, # of pets, date when last updated, etc)
+
+module.exports = router;
