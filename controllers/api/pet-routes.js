1a46c4024688d6e67e966867a1fe2b137e07d060
kieferjackson <kieferleejackson@gmail.com>
2022-09-27 19:49:53 -0700

    Fixed file pathing + implemented GET pets by id

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
-const { Pet } = require('../models');
+const { Pet } = require('../../models');
+
+router.get('/:id', async (req, res) =>
+{
+    const given_pet_id = req.params.id;
+
+    try
+    {
+        const petData = await Pet.findByPk(given_pet_id);
+
+        if (!petData)
+        {
+            res.status(404).json({ message: `No pet with ID: ${given_pet_id}` });
+            return;
+        }
+
+        res.status(200).json(petData);
+    }
+    catch (error)
+    {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
 
 // TODO: POST a new pet
 router.post('/', async (req, res) =>
