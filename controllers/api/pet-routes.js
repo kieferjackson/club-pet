dcf6ddf7f7d6c7c7951347516206e01c50125701
kieferjackson <kieferleejackson@gmail.com>
2022-09-27 19:42:58 -0700

    Added post route for adding pets

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
-// TODO: include models here and change directory path if necessary >>> const { Pet } = require('../models');
+const { Pet } = require('../models');
 
 // TODO: POST a new pet
+router.post('/', async (req, res) =>
+{
+    try
+    {
+        const newPet = await Pet.create
+        (
+            {
+                pet_name: req.body.pet_name,
+                about_pet: req.body.about_pet,
+                owner_id: req.body.owner_id
+            }
+        );
+
+        req.status(200).json(newPet);
+    }
+    catch (error)
+    {
+        console.log(error)
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
