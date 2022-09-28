2d7de5dff234cbfaa5fad88037a83883aac7f2a8
Susan Jorgensen <suzi.jor2022@gmail.com>
2022-09-27 19:13:42 -0700

    added number of pets property to Parent.js

--- Models/Parent.js
+++ Models/Parent.js
@@ -17,7 +17,7 @@ Parent.Init(
         },
         pet_name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
         },
         about_pet: {
             type: DataTypes.STRING,
@@ -27,6 +27,10 @@ Parent.Init(
             type: DataTypes.STRING,
             allowNull: false,
         },
+        num_pet: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },  
         parent_id: {
             type: DataTypes.INTEGER,
             references: {
