96d7f1784667f46ed5d58311a73297367e494ef9
kieferjackson <kieferleejackson@gmail.com>
2022-09-29 13:29:47 -0700

    Added routes for updating/deleting pets

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -48,4 +48,59 @@ router.post('/', async (req, res) =>
     }
 });
 
+// Update preexisting pet
+router.put('/:id', async (req, res) =>
+{
+    const given_pet_id = req.params.id;
+
+    try
+    {
+        const petToUpdate = await Pet.findByPk(given_pet_id);
+
+        if (!petToUpdate)
+        {
+            res.status(404).json({ message: `No pet with ID: ${given_pet_id}` });
+            return;
+        }
+
+        // TODO: Check that the user has authentication to change this pet (they own this pet)
+
+        // Updated selected pet with the request body
+        petToUpdate.set(req.body);
+        await petToUpdate.save();
+
+        res.status(200).json(petToUpdate);
+    }
+    catch (error)
+    {
+        res.status(500).json(error);
+    }
+});
+
+router.delete('/:id', async (req, res) =>
+{
+    const given_pet_id = req.params.id;
+
+    try
+    {
+        const petToDelete = await Pet.findByPk(given_pet_id);
+
+        if (!petToDelete)
+        {
+            res.status(400).json({ message: `No pet with ID: ${given_pet_id}` });
+            return;
+        }
+
+        // TODO: Check that the user has authentication to delete this pet (they own this pet)
+
+        await petToDelete.destroy();
+
+        res.status(200).json({ message: `The pet with ID ${given_pet_id} was successfully removed from the database` });
+    }
+    catch (error)
+    {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
