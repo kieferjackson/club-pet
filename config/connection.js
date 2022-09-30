ef7e40f773afe2c96a495b20daf434b9a1ef1fab
Susan Jorgensen <suzi.jor2022@gmail.com>
2022-09-29 21:28:34 -0700

    Handlebars

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const sequelize = new Sequelize
 (
     process.env.DB_NAME,
-    process.env.USERNAME,
-    process.env.PASSWORD,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,10 @@
-const sequelize = require("../config/connection.js");
-const seedUser = require("../userData");
-const seedPet = require("../petData");
-const seedDatabase = require("../db");  // created value for seedDatabase 
-
+const sequelize = require("./config/connection.js");
+const seedUser = require("./userData");
+const seedPet = require("./petData"); 
 const userData = require("./userData.json");
 
 //CHECK seed files and values, check terminal to see if listening or connected  
-let seedDatabase = async () => {
+let seedAll = async () => {
     await sequelize.sync({ force: false }); //changed to false, to run tests
     
     await seedUser ();
