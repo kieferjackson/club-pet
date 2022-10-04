f15d0320c7a96853e6f61c4e1e73eb994d905071
kieferjackson <kieferleejackson@gmail.com>
2022-10-04 13:06:37 -0700

    Fixed seed files + seeded data

--- seeds/petData.js
+++ seeds/petData.js
@@ -1,10 +1,29 @@
-const { Pet } = require("./Modles");
+const { Pet } = require("../Models");
 
 const petData = [
     {
-        name: 'John Doe',
-        owner_id: '1',
-        pet_name:  'Doggo',        
+        pet_name: 'Cosmos',
+        about_pet: 'Such a great dog! One day, he hopes to be a jazz singer.',
+        sex:  'female',
+        likes: 1,
+        species_id: 1,
+        owner_id: 1    
+    },
+    {
+        pet_name: 'King',
+        about_pet: 'Did you know that Panda bears are just Polar bears with black spray paint?',
+        sex:  'male',
+        likes: 5,
+        species_id: 5,
+        owner_id: 1    
+    },
+    {
+        pet_name: 'Daisy',
+        about_pet: 'The last gift from Helen.',
+        sex:  'female',
+        likes: 17,
+        species_id: 1,
+        owner_id: 2    
     },
 ];
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,15 @@
-const sequelize = require("./config/connection.js");
+const sequelize = require("../config/connection.js");
 const seedUser = require("./userData");
+const seedSpecies = require("./speciesData"); 
 const seedPet = require("./petData"); 
-const userData = require("./userData.json");
 
 //CHECK seed files and values, check terminal to see if listening or connected  
 let seedAll = async () => {
     await sequelize.sync({ force: false }); //changed to false, to run tests
     
     await seedUser ();
+
+    await seedSpecies ();
 
     await seedPet ();
 
