52617887513247ece12d97815bb97d971c9babd5
kieferjackson <kieferleejackson@gmail.com>
2022-10-06 00:11:53 -0700

    Pet form now accessed through modal

---
+++ public/js/modal.js
@@ -0,0 +1,6 @@
+
+const modal_menu = document.querySelector('#modal_container')
+
+function setActive() { modal_menu.classList.add('is-active') }
+
+function setInactive() { modal_menu.classList.remove('is-active') }
